a05192eaac0d62baa9b63427d229232ec9112698
helar89 <helar_duran@hotmail.com>
2020-09-24 12:07:45 -0400

    update before heroku deploy

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
 const dotenv = require('dotenv')
 const db = require('./config/keys').mongoURI
+const path = require('path')
+const item = require('./routes/api/items')
+
+
 const app = express()
 
 
@@ -18,6 +22,23 @@ mongoose
 })
 .then(()=>console.log('MongoDB Connected....'))
 .catch(err => console.log(err))
+
+// Use Routes
+app.use('/api/items',item)
+
+// Serve static assets if in production
+
+if(process.env.NODE_ENV === 'production'){
+
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*',(req,res)=>{
+res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+
 
 const port = process.env.PORT || 5000
  app.listen(port,
